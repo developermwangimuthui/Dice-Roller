8d8f775a46a35b3c278f0637c2efcaadd28a7a47
developermwangimuthui <intelligentsoftwaresdev@gmail.com>
2020-03-15 10:50:23 +0300

    dice roller

---
+++ app/src/main/java/intelligentsoftwaresdev/com/MainActivity.kt
@@ -0,0 +1,39 @@
+package intelligentsoftwaresdev.com
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+//    This aids in helping applications not lag in the event that the application is big
+    lateinit var diceImage:ImageView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.setOnClickListener{
+//            Toast.makeText(this, getString(R.string.btn_clicked), Toast.LENGTH_SHORT).show()
+            rollDice()
+        }
+        diceImage = findViewById(R.id.dice_image)
+
+    }
+
+    private fun rollDice() {
+        val randomInt = Random().nextInt(6) +1
+        val drawableResource = when(randomInt){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 ->R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
+    }
+}
